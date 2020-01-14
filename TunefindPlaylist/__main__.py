69fc191383dd0c6362ef0cb5d8bbd2e39779a01e
Frytek <flubbet@gmail.com>
2020-01-14 16:46:58 +0100

    Moved oauth to top

--- TunefindPlaylist/__main__.py
+++ TunefindPlaylist/__main__.py
@@ -13,14 +13,16 @@ def main():
         return
     if len(sys.argv) >= 3:
         device_id = sys.argv[2]
-    scraper = SongsScraper(sys.argv[1])
-    playlist_name = scraper.get_playlist_name()
-    songs = scraper.get_songs()
-    songs_ids = scraper.get_songs_ids(songs)
+
     try:
         manager = PlaylistManager(device_id)
     except InvalidDeviceId as ex:
         manager = PlaylistManager(ex.valid_device_ids[0])
+
+    scraper = SongsScraper(sys.argv[1])
+    playlist_name = scraper.get_playlist_name()
+    songs = scraper.get_songs()
+    songs_ids = scraper.get_songs_ids(songs)
     manager.generate_playlist_from_songs(songs_ids, playlist_name)
     print("\nDone! Playlist was saved as '{}'".format(playlist_name))
 
